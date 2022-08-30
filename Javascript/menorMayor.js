be7901272520dd17ecf4c457644b609a646757ad
Mathias <mathias.araujo2310@gmail.com>
2022-08-29 21:17:20 -0300

    primer subida header

---
+++ Javascript/menorMayor.js
@@ -0,0 +1,12 @@
+function Rango(menor, mayor){
+    let i = menor;
+    while(i<=mayor){
+        console.log(i);
+        i++
+    }
+}
+
+function menor(a,b,c){
+    return console.log(Math.min(a,b,c))
+}
+
